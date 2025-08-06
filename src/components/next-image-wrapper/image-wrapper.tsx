963acc8323827451219b658e4ab9be5a678f7300
Ryan McGuiness <ryanmcguiness123@gmail.com>
2025-08-06 16:26:20 -0400

    fixed console warning

--- src/components/next-image-wrapper/image-wrapper.tsx
+++ src/components/next-image-wrapper/image-wrapper.tsx
@@ -5,7 +5,7 @@ import cn from "classnames";
 const ImageWrapper = ({ src, alt, wrapperClassName, imageClassName }: { src: string; alt: string; wrapperClassName?: string, imageClassName?: string }) => {
     return (
         <div className={cn(s['wrapper'], wrapperClassName)}>
-            <Image src={src} alt={alt} className={cn(s['image'], imageClassName)} fill style={{ objectFit: 'cover' }} />
+            <Image src={src} alt={alt} className={cn(s['image'], imageClassName)} fill sizes="100%" style={{ objectFit: 'cover' }} />
         </div>
     );
 };
